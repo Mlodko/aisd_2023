5bf640017bd9ee8d76ed8355609b70b86b55aca3
Mlodkoo <63650085+Mlodko@users.noreply.github.com>
2023-05-16 14:54:59 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- lab_07/zad_02_precompute.py
+++ lab_07/zad_02_precompute.py
@@ -8,29 +8,6 @@ FILE_NUMBERS = [1000]
 FILES = [f'./patterns/{FILE_NUMBERS[i]}_pattern.txt' for i in range(len(FILE_NUMBERS))]
 
 
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    print ("\033[A                             \033[A")
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-    # Print New Line on Complete
-    if iteration == total:
-        print()
-
-
 def hash(string):
     hash_value = 0
     for i in range(len(string)):
@@ -46,7 +23,6 @@ def precompute_hashes(lines):
         iteration += 1
         if iteration % 1000 == 0:
             print(f'{iteration}/{total_hashes} done')
-        #printProgressBar(y, total_hashes)
         for x in range(len(lines[y]) - 2):
             to_hash = lines[y][x] + lines[y][x+1] + lines[y][x+2] + lines[y+1][x] + lines[y+2][x]
             #print(to_hash)
@@ -74,7 +50,6 @@ def hash_files():
         print(f'Precomputing hashes complete')
         hash_file = open(f'./hashes/{FILE_NUMBERS[i]}_hashes.txt', 'w')
         for i in range(len(hashes)):
-            #printProgressBar(i, len(hashes))
             hash_file.write(str(hashes[i]) + '\n')
         hash_file.close()
 
